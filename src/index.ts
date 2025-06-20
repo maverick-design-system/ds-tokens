3e02f7ae3b35ddc3d967c55fec01e9a55d6ba8a7
ShogoMark <trigmark87@gmail.com>
2025-06-20 11:06:22 +0100

    feat: initial tokens (colors, spacing, typography)

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as colors } from '../src/tokens/colors';
+export { default as spacing } from '../src/tokens/spacing';
+export { default as typography } from '../src/tokens/typography';
---
+++ src/tokens/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  primary: {
+    DEFAULT: "#3B82F6",
+    dark: "#1E40AF",
+    light: "#93C5FD",
+  },
+  gray: {
+    100: "#F3F4F6",
+    900: "#111827",
+  },
+};
+
+export default colors;
